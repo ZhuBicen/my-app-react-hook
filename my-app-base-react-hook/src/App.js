5f61ff1c764784a4df71d8643eff1f2dcff405ec
bicen.zhu <bicen.zhu@nokia-sbell.com>
2020-05-29 14:52:28 +0800

    useState, useEffect

--- my-app-base-react-hook/src/App.js
+++ my-app-base-react-hook/src/App.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import TokenForm from './TokenForm'
-export default class App extends Component {
 
-  state = {
-    token: null
-  };
 
-  componentDidMount() {
-    this.setState({ token: sessionStorage.getItem("token") });
-  }
 
-  setToken = token => {
+export default function App() {
+
+  const [token, setToken]  = useState(sessionStorage.getItem("token"));
+
+  useEffect(()=> {
     sessionStorage.setItem("token", token);
-    this.setState({ token });
-  }
+  })
 
-  render() {
-    const { token } = this.state;
-    return (
-      <div>
-        <h1>Starrry Eyed</h1>
-        {token ? token : <TokenForm setToken={this.setToken} />}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Starrry Eyed</h1>
+      {token ? token : <TokenForm setToken={setToken} />}
+    </div>
+  )
 
-};
+}
--- my-app-base-react-hook/src/TokenForm.js
+++ my-app-base-react-hook/src/TokenForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from "prop-types"
 
 export default class TokenForm extends React.Component {
